2fcd56f7290db3441c419a12fabc844a867cdd81
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-05-16 15:09:41 -0700

    poking around with with getting response

--- app/models/boba.rb
+++ app/models/boba.rb
@@ -1,9 +1,13 @@
 class Boba
 
-  def search(term, location)  # Modify so that it only takes in location// hard code the term
-    response = HTTPARTY.get("https://api.yelp.com/v3/businesses/search",
+  def search(location)  # Modify so that it only takes in location// hard code the term
+    response = HTTPARTY.get("https://api.yelp.com/v3/businesses/search?term=bubble tea&location=#{location}",
       headers = {'Authorization' => 'Bearer #{api_key}'}
     )
+# search query of just location somewhere
+binding.pry
+
+    results = JSON.parse(response.body)['businesses']['name']
   end
 end
 # def search(term, location)
