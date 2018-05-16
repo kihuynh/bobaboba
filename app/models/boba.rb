7428f2446ade610930caf48c9163c6b84208c7c3
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-05-16 15:13:38 -0700

    made headers into a symbol

--- app/models/boba.rb
+++ app/models/boba.rb
@@ -2,7 +2,7 @@ class Boba
 
   def search(location)  # Modify so that it only takes in location// hard code the term
     response = HTTPARTY.get("https://api.yelp.com/v3/businesses/search?term=bubble tea&location=#{location}",
-      headers = {'Authorization' => 'Bearer #{api_key}'}
+      :headers => {'Authorization' => 'Bearer #{api_key}'}
     )
 # search query of just location somewhere
 binding.pry
@@ -10,14 +10,3 @@ binding.pry
     results = JSON.parse(response.body)['businesses']['name']
   end
 end
-# def search(term, location)
-#   url = "#{API_HOST}#{SEARCH_PATH}"
-#   params = {
-#     term: term,
-#     location: location,
-#     limit: SEARCH_LIMIT
-#   }
-#
-#   response = HTTP.auth("Bearer #{API_KEY}").get(url, params: params)
-#   response.parse
-# end
